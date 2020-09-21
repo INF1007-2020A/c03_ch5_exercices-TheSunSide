9637daa6d5e884fd9713164a40444bd5fdd43967
TheSunSide <sunnee17@me.com>
2020-09-21 18:59:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,8 +27,9 @@ def prime_integer_summation() -> int:
 
 def factorial(number: int) -> int:
      cpt = 1
-     whilecpt <= number:
-
+     while cpt <= number:
+         
+     
     
     return 0
 
